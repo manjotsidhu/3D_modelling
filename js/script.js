96ed4bd9656b1b83d5cd528f1b84564ca39f1150
Manjot Sidhu <manjot.singh@btech.christuniversity.in>
2019-07-24 20:10:43 +0530

    Including three.js lib and function templates

--- js/script.js
+++ js/script.js
@@ -1,1 +1,31 @@
 /* Custom javascript for index.html */
+
+// Canvas
+const canvas = document.getElementById("mainCanvas");
+
+// Camera
+let camera;
+
+// Orbit Controls
+let controls;
+
+// THREE.Scene
+let scene;
+
+// THREE.WebGLRenderer
+let renderer;
+
+// init function loads the startup scripts on window load
+function init() {
+    // TODO
+}
+
+// animate function renders everytime the screen is refreshed to produce animation
+function animate() {
+    // TODO
+}
+
+// render function will render THREE.js scene
+function render() {
+    // TODO
+}
