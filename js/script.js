b35668cd86d98ea24b9f90edd38157ca6b72bde7
UNIFORMatom <52819025+UNIFORMatom@users.noreply.github.com>
2019-07-27 10:48:01 +0530

    Including the elements in init()

--- js/script.js
+++ js/script.js
@@ -5,40 +5,46 @@ const canvas = document.getElementById("mainCanvas");
 
 // Camera
 let camera;
-camera = new THREE.PerspectiveCamera(75,window.innerWidth / window.innerHeight,0.1,100);
-camera.position.set(0,0,10);
 
 // Orbit Controls
 let controls;
-controls = new THREE.OrbitControls(camera, renderer.domElement);
-controls.minDistance = 0;
-controls.maxDistance = 100000;
-
 
 // THREE.Scene
 let scene;
 
 // THREE.WebGLRenderer
 let renderer;
-renderer = new THREE.WebGLRenderer({antialias: true});
-renderer.setClearColor("#A9A9A9");
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
 
-document.addEventListener('resize', function(){
-        renderer.setSize(window.innerWidth, window.innerHeight);
-        camera.aspect = window.innerWidth / window.innerHeight;
-        camera.updateProjectionMatrix();
-});
+// init function loads the startup scripts on window load
+function init() {
+	camera = new THREE.PerspectiveCamera(85, window.innerWidth / window.innerHeight, 0.1, 2000);
+	camera.position.set( 0, 0, 10);
 
-//GLTF loader
- var loader = new THREE.GLTFLoader();
+	scene = new THREE.Scene();
 
-    loader.load( '3DModels/IR1_COMPRESSOR_.gltf', function ( gltf ) {
+	renderer = new THREE.WebGLRenderer({canvas,
+		antialias: true});
+	renderer.setClearColor("#A9A9A9");
+	renderer.setSize(window.innerWidth, window.innerHeight);
+	document.body.appendChild(renderer.domElement);
+
+	controls = new THREE.OrbitControls(camera, renderer.domElement);
+	controls.minDistance = 0;
+	controls.maxDistance = 100000;
+
+	document.addEventListener('resize', function(){
+			renderer.setSize(window.innerWidth, window.innerHeight);
+			camera.aspect = window.innerWidth / window.innerHeight;
+			camera.updateProjectionMatrix();
+	});
+
+	var loader = new THREE.GLTFLoader();
+
+    loader.load( '../models/MODEL_IR_ORIGIN1.gltf', function ( gltf ) {
 
         var compressor = gltf.scene;
         compressor.scale.set(1,1,1);
-        var compressor_material = new THREE.MeshBasicMaterial({map: new THREE.TextureLoader().load("3DModel/Textures/Fencing_Diamond_Mesh.png"), side: THREE.DoubleSide});
+        var compressor_material = new THREE.MeshBasicMaterial({map: new THREE.TextureLoader().load("../textures/Fencing_Diamond_Mesh.png"), side: THREE.DoubleSide});
         scene.add( compressor );
         render();
 
@@ -48,21 +54,17 @@ document.addEventListener('resize', function(){
 
     } );
 
-//Lights
-var spotLight = new THREE.SpotLight(0xFFFFFF,1);
-spotLight.position.set(30,40,20);
-scene.add(spotLight);
+	var spotLight = new THREE.SpotLight(0xFFFFFF,1);
+	spotLight.position.set(30,40,20);
+	scene.add(spotLight);
 
-var spotLight1 = new THREE.SpotLight(0xFFFFFF,1);
-spotLight1.position.set(-30,-40,-20);
-scene.add(spotLight1);
+	var spotLight1 = new THREE.SpotLight(0xFFFFFF,1);
+	spotLight1.position.set(-30,-40,-20);
+	scene.add(spotLight1);
 
-var ambientLight = new THREE.AmbientLight(0xFFFFFF,0.4);
-scene.add(ambientLight);
-
-// init function loads the startup scripts on window load
-function init() {
-    // TODO
+	var ambientLight = new THREE.AmbientLight(0xFFFFFF,0.4);
+	scene.add(ambientLight);
+	
 }
 
 // animate function renders everytime the screen is refreshed to produce animation
