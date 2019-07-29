1d8899bb7a9e6eb9a84826e78bf7b367f294b043
Manjot Sidhu <manjot.singh@btech.christuniversity.in>
2019-07-29 13:27:50 +0530

    Bringup realtime values using firebase database

---
+++ js/firebaseConfig.js
@@ -0,0 +1,10 @@
+// Firebase Configuration File
+var firebaseConfig = {
+    apiKey: "API-KEY-WILL-COME-HERE",
+    authDomain: "d-modelling-9755e-4496d.firebaseapp.com",
+    databaseURL: "https://d-modelling-9755e-4496d.firebaseio.com",
+    projectId: "d-modelling-9755e",
+    storageBucket: "",
+    messagingSenderId: "977607199046",
+    appId: "1:977607199046:web:c40c6e6b50a1ded4"
+}
---
+++ js/realtimeValues.js
@@ -0,0 +1,79 @@
+/* Gets realtime values from firebase and updates compUniforms */
+
+// Initialize firebase using firebaseConfig
+firebase.initializeApp(firebaseConfig);
+
+// Get a reference to the database service
+var database = firebase.database(); 
+
+// Comp1 colors reference to database
+var comp1Ref = firebase.database().ref("IR-Compressor/comp1");
+comp1Ref.on("value", function(snapshot) {
+    setCompUniforms(1, snapshot.val().value1, snapshot.val().value2);
+}, function (error) {
+    console.log("Error: " + error.code);
+});
+
+// Comp2 colors reference to database
+var comp2Ref = firebase.database().ref("IR-Compressor/comp2");
+comp2Ref.on("value", function(snapshot) {
+    setCompUniforms(2, snapshot.val().value1, snapshot.val().value2);
+}, function (error) {
+    console.log("Error: " + error.code);
+});
+
+// Comp3 colors reference to database
+var comp3Ref = firebase.database().ref("IR-Compressor/comp3");
+comp3Ref.on("value", function(snapshot) {
+    setCompUniforms(3, snapshot.val().value1, snapshot.val().value2);
+}, function (error) {
+    console.log("Error: " + error.code);
+});
+
+// Comp4 colors reference to database
+var comp4Ref = firebase.database().ref("IR-Compressor/comp4");
+comp4Ref.on("value", function(snapshot) {
+    setCompUniforms(4, snapshot.val().value1, snapshot.val().value2);
+}, function (error) {
+    console.log("Error: " + error.code);
+});
+
+// Comp5 colors reference to database
+var comp5Ref = firebase.database().ref("IR-Compressor/comp5");
+comp5Ref.on("value", function(snapshot) {
+    setCompUniforms(5, snapshot.val().value1, snapshot.val().value2);
+}, function (error) {
+    console.log("Error: " + error.code);
+});
+
+// Comp6 colors reference to database
+var comp6Ref = firebase.database().ref("IR-Compressor/comp6");
+comp6Ref.on("value", function(snapshot) {
+    setCompUniforms(6, snapshot.val().value1, snapshot.val().value2);
+}, function (error) {
+    console.log("Error: " + error.code);
+});
+
+// Comp7 colors reference to database
+var comp7Ref = firebase.database().ref("IR-Compressor/comp7");
+comp7Ref.on("value", function(snapshot) {
+    setCompUniforms(7, snapshot.val().value1, snapshot.val().value2);
+}, function (error) {
+    console.log("Error: " + error.code);
+});
+
+// Comp1 colors reference to database
+var comp8Ref = firebase.database().ref("IR-Compressor/comp8");
+comp8Ref.on("value", function(snapshot) {
+    setCompUniforms(8, snapshot.val().value1, snapshot.val().value2);
+}, function (error) {
+    console.log("Error: " + error.code);
+});
+
+// Comp1 colors reference to database
+var comp9Ref = firebase.database().ref("IR-Compressor/comp9");
+comp9Ref.on("value", function(snapshot) {
+    setCompUniforms(9, snapshot.val().value1, snapshot.val().value2);
+}, function (error) {
+    console.log("Error: " + error.code);
+});
